b74eae14fbe46784b7d76012be70de70618b49cb
Janvi0410 <100525353+Janvi0410@users.noreply.github.com>
2024-06-16 19:53:40 +0530

    Add files via upload

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cartitems from '../components/CartItems/Cartitems'
+
+const Cart = () => {
+  return (
+    <div>
+      <Cartitems/>
+    </div>
+  )
+}
+
+export default Cart
