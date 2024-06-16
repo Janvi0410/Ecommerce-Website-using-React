ca62dbbfa121b43185772e075e44aee9e999c989
Janvi0410 <100525353+Janvi0410@users.noreply.github.com>
2024-06-16 19:52:25 +0530

    Add files via upload

---
+++ src/components/Assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.jpg'
+import p2_img from './product_2.jpg'
+import p3_img from './product_3.jpg'
+import p4_img from './product_4.jpg'
+
+let data_product = [
+  {
+    id:1,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p1_img,
+    new_price:"₹499",
+    old_price:"₹799",
+  },
+  {id:2,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p2_img,
+    new_price:"₹899",
+    old_price:"₹999",
+  },
+  {id:3,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p3_img,
+    new_price:"₹1299",
+    old_price:"₹1499",
+  },
+  {id:4,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p4_img,
+    new_price:"₹789",
+    old_price:"₹999",
+  },
+];
+
+export default data_product;
---
+++ src/components/DescriptionBox/Descriptionbox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Descriptionbox.css'
+
+const Descriptionbox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+      <div className="descriptionbox-nav-box">Description</div>
+      <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>An ecommerce website is an online platform designed to facilitate the buying and selling of products and services. It features a user-friendly interface that allows customers to browse product categories, view detailed product information, and make purchases through a secure checkout process. Key functionalities include a shopping cart, user account management, search and filter options, and customer support through live chat or contact forms. The website is optimized for various devices and incorporates security measures like SSL encryption and secure payment gateways. Additionally, it supports marketing activities such as email campaigns, social media integration, and SEO optimization to attract and retain customers.</p>
+        <p>
+        By incorporating these features and principles, an ecommerce website can provide a robust, secure, and enjoyable shopping experience for users while efficiently managing the backend operations for administrators.  
+        </p>
+      </div>
+      </div>
+    
+  )
+}
+
+export default Descriptionbox
